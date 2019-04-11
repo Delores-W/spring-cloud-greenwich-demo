9b44762314b7af381088bb7af418f515a2453518
william <wangjm@iata.org>
2019-04-11 16:32:29 +0800

    forward

---
+++ config-client-auto-refresh/src/main/java/com/galaxy/ConfigClientAutoRefreshApplication.java
@@ -0,0 +1,11 @@
+package com.galaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAutoRefreshApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientAutoRefreshApplication.class, args);
+    }
+}
--- config-server-auto-refresh/src/main/java/com/galaxy/ConfigServerAutoRefreshApplication.java
+++ config-server-auto-refresh/src/main/java/com/galaxy/ConfigServerAutoRefreshApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerAutoRefreshApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerAutoRefreshApplication.class, args);
     }
 }
